5edb3d9966f60550ba7c076e88b36991e24ba930
John Pusey <ebardx@gmail.com>
2024-10-22 18:08:13 -0400

    WIP

--- Sources/XestiMath/RealFormatStyle+Internal.swift
+++ Sources/XestiMath/RealFormatStyle+Internal.swift
@@ -12,7 +12,17 @@ extension RealFormatStyle {
     }
 
     internal func formatDouble(_ value: Double) -> String {
-        ""
+        var fs = FloatingPointFormatStyle.localizedDouble(locale: locale)
+
+        if alwaysDisplaysFloatingPointSeparator {
+            fs = fs.decimalSeparator(strategy: .always)
+        }
+
+        if !displaysGrouping {
+            fs = fs.grouping(.never)
+        }
+
+        return fs.format(value)
     }
 
     internal func formatFraction(_ value: Fraction) -> String {
